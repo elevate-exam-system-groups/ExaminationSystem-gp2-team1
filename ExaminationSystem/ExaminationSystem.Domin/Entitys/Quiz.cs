e30a7df0255543c7af94fbd786e8fc82c85879d5
mosaleh55 <www.mosaleh@gmail.com>
2026-04-11 09:02:40 +0300

    Remove bin and obj from tracking

---
+++ ExaminationSystem/ExaminationSystem.Domin/Entitys/Quiz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExaminationSystem.Domin.Comman;
+using ExaminationSystem.Domin.Enums;
+
+namespace ExaminationSystem.Domin.Entitys
+{
+    public class Quiz : BaseEntity
+    {
+        public string Title { get; set; } = null!;
+
+        public int DiplomaId { get; set; }
+        public Diploma Diploma { get; set; }
+
+        public int DurationMinutes { get; set; }
+        public int PassScore { get; set; }
+        public int? MaxAttempts { get; set; }
+
+        public QuizStatus Status { get; set; }
+        public string ? Instructions { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+ 
+
+        public virtual ICollection<Question> Questions { get; set; } = new HashSet<Question>();
+        public virtual ICollection<Attempt> Attempts { get; set; } = new HashSet<Attempt>();
+
+    }
+}
