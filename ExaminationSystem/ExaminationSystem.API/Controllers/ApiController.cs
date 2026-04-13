e45ffdcb6a15f9dec31b643058479883879f64b5
mosaleh55 <www.mosaleh@gmail.com>
2026-04-13 09:26:16 +0300

    Refactor Result pattern and add error handling support
    Refactored Result.cs to introduce a new Result<TValue> class with improved error handling and implicit conversions. Added record structs for operation results. Introduced Error.cs for error representation and ApiController.cs for standardized API error responses. Removed obsolete folder reference from the project file.

---
+++ ExaminationSystem/ExaminationSystem.API/Controllers/ApiController.cs
@@ -0,0 +1,42 @@
+﻿
+
+using ExaminationSystem.Domin.Comman.Result;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace ExaminationSystem.Api.Controllers;
+
+[ApiController]
+public class ApiController : ControllerBase
+{
+    protected ActionResult Problem(List<Error> errors)
+    {
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
+
+       /* if (errors.All(error => error.Type == ErrorKind.Validation))
+        {
+            return ValidationProblem(errors);
+        }*/
+
+        return Problem(errors[0]);
+    }
+
+    private ObjectResult Problem(Error error)
+    {
+     
+        int errorCode = (int)error.Code;
+        return Problem(statusCode: errorCode, title: error.Description);
+    }
+
+   /* private ActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        errors.ForEach(error => modelStateDictionary.AddModelError(error.Code, error.Description));
+
+        return ValidationProblem(modelStateDictionary);
+    }*/
+}
