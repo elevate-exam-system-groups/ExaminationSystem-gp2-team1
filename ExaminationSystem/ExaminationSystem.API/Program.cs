4b023937ec8c7f91f104bb3a40c81ed88faf8866
mohamdkabeel <kabeelmohamed22@gmail.com>
2026-04-15 00:00:42 +0200

    implement diploma quizzes endpoint (US 2.3)

--- ExaminationSystem/ExaminationSystem.API/Program.cs
+++ ExaminationSystem/ExaminationSystem.API/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddOpenApi();
 builder.Services.AddPresentation(builder.Configuration)
                 .AddApplication()
                 .AddInfrastructure(builder.Configuration);
+builder.Services.AddScoped<IDiplomaQuizService, DiplomaQuizService>();
+builder.Services.AddAutoMapper(typeof(DiplomaQuizMappingProfile));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,5 +26,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.Run();
---
+++ ExaminationSystem/ExaminationSystem.Application/Dtos/Quizzes/DiplomaQuizDto.cs
@@ -0,0 +1,12 @@
+﻿namespace testexamination.Dtos
+{
+    public class DiplomaQuizDto
+    {
+        public int QuizId { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int TotalQuestions { get; set; }
+        public double PassingScore { get; set; }
+        public List<QuizAttemptDto> ScoreHistory { get; set; } = new();
+    }
+}
