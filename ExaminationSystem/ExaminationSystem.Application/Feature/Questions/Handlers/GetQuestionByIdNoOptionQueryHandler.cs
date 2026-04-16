9f8efe215d0171985bd0abafb9c5ae1bac5c4162
ahmed khfaga <idkmaster89@gmail.com>
2026-04-17 00:21:36 +0200

    refactor Query GetQuestion from all option to no option

---
+++ ExaminationSystem/ExaminationSystem.Application/Feature/Questions/Handlers/GetQuestionByIdNoOptionQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExaminationSystem.Application.Common.DTOs.OptionsDTOs;
+using ExaminationSystem.Application.Common.DTOs.QuestionsDTOs;
+using ExaminationSystem.Domin.Comman.Result;
+using ExaminationSystem.Domin.Common.Result;
+using ExaminationSystem.Domin.Contracts;
+using ExaminationSystem.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExaminationSystem.Application.Feature.Questions.Handlers
+{
+    public class GetQuestionByIdNoOptionQueryHandler(IGenericRepository<Question> _questionRepo) : IRequestHandler<GetQuestionByIdNoOptionQuery, RequestResult<QuestionQuizDto>>
+    {
+        public async Task<RequestResult<QuestionQuizDto>> Handle(GetQuestionByIdNoOptionQuery request, CancellationToken cancellationToken)
+        {
+            var question = await _questionRepo.GetAll()
+                .Where(q => q.Id == request.QuestionId)
+                .Select(q => new QuestionQuizDto
+                {
+                    QuestionId = q.Id,
+
+                    Text = q.Text,
+                    QuizId = q.QuizId,                    
+                }).FirstOrDefaultAsync(cancellationToken);
+
+
+            if (question is null)
+            {
+                return RequestResult<QuestionQuizDto>.Failure(ErrorCode.NotFound, "Question not found");
+            }
+
+            return RequestResult<QuestionQuizDto>.Sucess(question);
+        }
+    }
+}
