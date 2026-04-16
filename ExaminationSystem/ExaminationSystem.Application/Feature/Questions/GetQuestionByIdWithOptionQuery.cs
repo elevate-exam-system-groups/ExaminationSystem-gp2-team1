7aaec66f7f283836ad714d9d95d6fca2f3da8bcc
ahmed khfaga <idkmaster89@gmail.com>
2026-04-17 00:23:07 +0200

    adding new query get all lquestion with options

---
+++ ExaminationSystem/ExaminationSystem.Application/Feature/Questions/GetQuestionByIdWithOptionQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExaminationSystem.Application.Common.DTOs.QuestionsDTOs;
+using ExaminationSystem.Domin.Common.Result;
+using MediatR;
+
+namespace ExaminationSystem.Application.Feature.Questions;
+
+public record GetQuestionByIdWithOptionQuery(Guid QuestionId) : IRequest<RequestResult<QuestionQuizDto>>;
+
