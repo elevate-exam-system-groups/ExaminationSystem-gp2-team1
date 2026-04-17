2a711ab949b89216b20a79c90d1c83cf338d7fc3
ahmed khfaga <idkmaster89@gmail.com>
2026-04-17 14:22:09 +0200

    updated orcherstrator

---
+++ ExaminationSystem/ExaminationSystem.Application/Feature/Quizzes/Orcherstrator/StartQuiz/StartQuizOrcherstrator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExaminationSystem.Application.DTOs.QuizzesDTOs;
+using ExaminationSystem.Domin.Common.Result;
+using MediatR;
+
+namespace ExaminationSystem.Application.Feature.Quizzes.Command.StartQuiz;
+
+public record StartQuizOrcherstrator(Guid QuizId, Guid UserId) : IRequest<RequestResult<StartQuizResponse>>;
+
---
+++ ExaminationSystem/ExaminationSystem.Application/Feature/Quizzes/Orcherstrator/StartQuiz/StartQuizOrcherstratorHandler.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExaminationSystem.Application.Common.DTOs.OptionsDTOs;
+using ExaminationSystem.Application.Common.DTOs.QuestionsDTOs;
+using ExaminationSystem.Application.DTOs.QuizzesDTOs;
+using ExaminationSystem.Application.Feature.Quizzes.Query;
+using ExaminationSystem.Domin.Comman.Result;
+using ExaminationSystem.Domin.Common.Result;
+using ExaminationSystem.Domin.Contracts;
+using ExaminationSystem.Domin.Entities.Enums;
+using ExaminationSystem.Entities;
+using MediatR;
+
+namespace ExaminationSystem.Application.Feature.Quizzes.Command.StartQuiz;
+
+public class StartQuizOrcherstratorHandler(IMediator _mediator) : IRequestHandler<StartQuizOrcherstrator, RequestResult<StartQuizResponse>>
+{
+    public async Task<RequestResult<StartQuizResponse>> Handle(StartQuizOrcherstrator request, CancellationToken cancellationToken)
+    {
+        // get time of starting the quiz
+        var now = DateTime.UtcNow;
+
+        // need get quiz 
+        var quiz = await _mediator.Send(new GetQuizByIdQuery(request.QuizId),cancellationToken);
+
+        if (!quiz.IsSucess) 
+        {
+            return RequestResult<StartQuizResponse>.Failure(ErrorCode.NotFound, "Quiz not found");
+        }
+
+        var quizData = quiz.Data;
+
+        // check existing attempt for the user and quiz
+        var existingAttempt = await _mediator.Send(new GetInProgressAttemptQuery(request.QuizId, request.UserId),cancellationToken);
+        if (existingAttempt.Data != null)
+        {
+            return RequestResult<StartQuizResponse>.Failure(
+                ErrorCode.ExistingAttempt,
+                $"You already have an active attempt with id {existingAttempt.Data}");
+        }
+            
+        // check max attempts
+        var hasReachedLimit = await _mediator.Send(new CheckMaxAttemptQuery(request.QuizId,request.UserId,quizData.MaxAttempts),cancellationToken);
+
+        if (hasReachedLimit)
+        {
+            return RequestResult<StartQuizResponse>.Failure(
+                ErrorCode.LimitReached,
+                "Attempt limit reached");
+        }
+
+
+        //var attempt = new QuizAttempt
+        //{
+        //    Id = Guid.NewGuid(),
+        //    StudentId = request.UserId,
+        //    QuizId = request.QuizId,
+        //    Status = QuizAttemptStatus.inProgress,
+        //    StartedAt = now,
+        //    TotalQuestions = quizData.Questions.Count // this row is importand >?? is that from meta data of quiz or from attempt answer count ??
+        //};
+        //_unitOfWork.GetRepository<QuizAttempt>().Add(attempt);
+        //await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+
+
+        ///
+        /// need look here again to test 
+        ///
+        var attempt = await _mediator.Send(new CreateQuizAttemptCommand(request.UserId,request.QuizId));
+
+        if (attempt.IsSucess) 
+        {
+            return RequestResult<StartQuizResponse>.Failure(ErrorCode.Conflict, "Failed to create quiz attempt");
+        }
+        var attemptData = attempt.Data;
+
+
+        /// shuffle questions and options
+        /// need adding private method to shuffle list of questions
+        var questions = quizData.Questions.OrderBy(q => Guid.NewGuid())
+        .Select(q => new QuestionDto
+        {
+            Id = q.Id,
+            Text = q.Text,
+            Options = q.Options
+                        .OrderBy(o => Guid.NewGuid())
+                        .Select(o => new OptionDto
+                        {
+                            Id = o.Id,
+                            Text = o.Text
+                        }).ToList()
+        }).ToList();
+
+        var response = new StartQuizResponse
+        {
+            AttemptId = attemptData.Id,
+            Duration = quizData.DurationMinutes,
+            Questions = questions
+        };
+
+        return RequestResult<StartQuizResponse>.Sucess(response);
+    }
+}
