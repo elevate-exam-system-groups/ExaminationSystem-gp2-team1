3f0ff82f78b118cd0f1ebe20bce82423c05162de
ahmed khfaga <idkmaster89@gmail.com>
2026-04-23 18:33:33 +0200

    write quizhandler by id

--- ExaminationSystem/ExaminationSystem.Application/Feature/Quizzes/Query/GetQuizByIdQueryHandler.cs
+++ ExaminationSystem/ExaminationSystem.Application/Feature/Quizzes/Query/GetQuizByIdQueryHandler.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Text;
 using ExaminationSystem.Application.Common.DTOs.QuizzesDTOs;
+using ExaminationSystem.Domin.Comman.Result;
 using ExaminationSystem.Domin.Common.Result;
+using ExaminationSystem.Domin.Contracts;
+using ExaminationSystem.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExaminationSystem.Application.Feature.Quizzes.Query
 {
-    public class GetQuizByIdQueryHandler : IRequestHandler<GetQuizByIdQuery, RequestResult<QuizDto>>
+    public class GetQuizByIdQueryHandler(IGenericRepository<Quiz> _quizRepo) : IRequestHandler<GetQuizByIdQuery, RequestResult<QuizDto>>
     {
-        Task<RequestResult<QuizDto>> IRequestHandler<GetQuizByIdQuery, RequestResult<QuizDto>>.Handle(GetQuizByIdQuery request, CancellationToken cancellationToken)
+        async Task<RequestResult<QuizDto>> IRequestHandler<GetQuizByIdQuery, RequestResult<QuizDto>>.Handle(GetQuizByIdQuery request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var quiz = await _quizRepo.GetAll().Where(q => q.Id == request.QuizId)
+                .Select(q => new QuizDto 
+                {
+                    Id = q.Id,
+                    Title = q.Title,
+                    DurationMinutes = q.DurationMinutes,
+                    MaxAttempts = q.MaxAttempts
+
+                }).FirstOrDefaultAsync(cancellationToken);
+
+            if (quiz == null) 
+            {
+                return RequestResult<QuizDto>.Failure(ErrorCode.NotFound, "Quiz not found");
+            }
+            return RequestResult<QuizDto>.Sucess(quiz);
         }
     }
 }
