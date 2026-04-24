29308b7945f20e9fd218c3c6d52dd4505ac2a0f5
ahmed khfaga <idkmaster89@gmail.com>
2026-04-24 15:06:22 +0300

    adding dtos for analytics

---
+++ ExaminationSystem/ExaminationSystem.Application/Common/DTOs/AnalyticsDTOs/PassRateDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExaminationSystem.Application.Common.DTOs.AnalyticsDTOs
+{
+    public class PassRateDto
+    {
+        public Guid QuizId { get; set; }
+        public string QuizTitle { get; set; } 
+        public double PassRate { get; set; }
+    }
+}
